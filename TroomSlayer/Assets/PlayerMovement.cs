7b677904a47fa3052717362a764a4fcb5caef621
Alexandre Dorne <125374011+alexdorne@users.noreply.github.com>
2024-10-03 17:26:32 +0100

    Imported textmeshpro and some scripting

--- TroomSlayer/Assets/PlayerMovement.cs
+++ TroomSlayer/Assets/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
 
     // FLOATS 
 
+    private float MoveSpeed { get; set; }
+
+    private float horizontalInput;
+    private float verticalInput; 
 
     // INTS 
 
@@ -20,13 +24,26 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        
+        GatherInput();
+
+        Movement(); 
     }
 
+    private void GatherInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical"); 
+    }
+
+    private void Movement()
+    {
+        Vector3 movement = new Vector3(MoveSpeed * horizontalInput * Time.deltaTime, 0, MoveSpeed * verticalInput * Time.deltaTime); 
+        //rb.angularVelocity = Vector3.Lerp()
+    }
 
 }
