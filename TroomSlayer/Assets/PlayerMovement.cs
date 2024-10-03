95fdd80996199c1339581b8a601b38cdebb1d05b
Alexandre Dorne <125374011+alexdorne@users.noreply.github.com>
2024-10-03 16:37:12 +0100

    Project Organisation

---
+++ TroomSlayer/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // REFERENCES 
+
+    Rigidbody rb;
+
+
+
+
+    // FLOATS 
+
+
+    // INTS 
+
+
+    // BOOLEANS 
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+}
